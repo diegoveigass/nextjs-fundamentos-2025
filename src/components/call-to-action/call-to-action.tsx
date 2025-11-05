e6df31f6a6cadb693046afe5953692e106fba072
Diego Veiga <diegoveigass@gmail.com>
2025-11-05 18:05:41 -0300

    Adding background images

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -4,9 +4,11 @@ import { Button } from "../ui/button";
 
 export const CallToAction = () => {
 	return (
-		<section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700 relative">
-			<div className="container flex flex-col items-center gap-6 text-center">
-				<div className="bg-cyan-300 size-16 rounded-full flex items-center justify-center absolute left-1/2 -translate-x-1/2 -top-8">
+		<section className="py-24 bg-gradient-to-b relative">
+			<div className="absolute inset-0 hidden md:block bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+
+			<div className="container flex flex-col items-center gap-6 text-center relative">
+				<div className="bg-cyan-300 size-16 rounded-full flex items-center justify-center">
 					<Store className="text-cyan-100 size-8" />
 				</div>
 				<h2 className="text-heading-xl text-gray-100 text-balance font-sans">
