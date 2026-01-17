533e72acc2631320efce97fafc65921326e4be61
Diego Veiga <sdiegoveiga@gmail.com>
2026-01-17 14:51:27 -0300

    Creating not found page in app router

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -2,12 +2,14 @@
 
 import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export function Search() {
+	const inputRef = useRef<HTMLInputElement | null>(null);
 	const router = useRouter();
 	const searchParams = useSearchParams();
 	const query = searchParams?.get("q") ?? "";
+	const hasQuery = searchParams?.has("q");
 
 	const handleSearch = useCallback(
 		(event: React.FormEvent) => {
@@ -19,6 +21,12 @@ export function Search() {
 		},
 		[query, router.push],
 	);
+
+	useEffect(() => {
+		if (hasQuery) {
+			inputRef.current?.focus();
+		}
+	}, [hasQuery]);
 
 	const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const newQuery = event.target.value;
@@ -41,6 +49,7 @@ export function Search() {
 				className="text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300 data-[query=true]:text-blue-300"
 			/>
 			<input
+				ref={inputRef}
 				type="text"
 				placeholder="Buscar"
 				value={query}
